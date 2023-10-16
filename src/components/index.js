632b489e4445eac2414c9f1bd598899ffa5f6d92
Ahmed Hamza <ahmedarif2882@gmail.com>
2023-10-16 16:51:06 +0500

    used React Hook Form

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,10 @@ import Footer from "./Footer/Footer";
 import Container from "./Container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
+import Input from './Input'
+import Button from './Button'
+import Login from './Login'
+import PostCard from './PostCard'
+import Select from './Select'
 
-export {Header, Footer, Container, Logo, LogoutBtn}
+export {Header, Footer, Container, Logo, LogoutBtn, Input, Button, Login, PostCard, Select}
